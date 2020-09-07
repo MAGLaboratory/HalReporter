d6e6c1ad5d213abc227bdfb33a43af978aed50e7
blu006 <blu006@ucr.edu>
2020-09-06 17:54:15 -0700

    Added better decodeing and event handling.

--- HalReporter.py
+++ HalReporter.py
@@ -27,14 +27,20 @@ class HR(mqtt.Client):
         data_sources: List[str]
         subtopics: List[str]
         boot_check_list: Dict[str, List[str]]
+        long_checkup_freq: int
+        long_checkup_leng: int
         mqtt_broker: str
         mqtt_port: int
+        checkup_freq: int
 
     checkups = {}
+    notify_checkup = {}
+    other_bootup = {}
     session = ''.encode('utf-8')
     version = 2020
     secret = ''
     topics = []
+    pings = 0
     
     def on_log(self, client, userdata, level, buff):
         if level != mqtt.MQTT_LOG_DEBUG:
@@ -57,11 +63,27 @@ class HR(mqtt.Client):
     def on_message(self, client, userdata, msg):
         print("Message received: " + msg.topic)
         for source in self.data.data_sources:
-            if msg.topic == source + '/' + "checkup":
-                self.checkups[source] = json.loads(msg.payload.decode())
+            if msg.topic == source + "/checkup":
+                self.checkups[source] = json.loads(msg.payload.decode('utf-8'))
                 return
+            if msg.topic == source + "/bootup":
+                if msg.payload.decode('utf-8') == '':
+                    print("Empty payload.")
+                    return
+                print(msg.payload.decode('utf-8'))
+                self.other_bootup.update(json.loads(msg.payload.decode('utf-8')))
+                self.publish(source + "/bootup", payload=None, retain=True)
+                return
+            if msg.topic == source + "/event":
+                event_dict = json.loads(msg.payload.decode('utf-8'))
+                for key in event_dict:
+                    try:
+                      self.checkups[source].pop(key, None)
+                    except KeyError:
+                      print("Key error on event.  Probably no checkup before event.")
+                self.notify('checkup', event_dict)
         if msg.topic in self.topics:
-            print(msg.topic + ":" + msg.payload.decode())
+            print(msg.payload.decode('utf-8'))
             return
     
     def get_secret(self):
@@ -102,28 +124,44 @@ class HR(mqtt.Client):
         print("Bootup:")
        
         for bc_name, bc_cmd in self.data.boot_check_list.items():
-            boot_checks[bc_name] = subprocess.check_output(bc_cmd, shell=True)
+            boot_checks[bc_name] = subprocess.check_output(bc_cmd, shell=True).decode('utf-8')
     
+        boot_checks.update(self.other_bootup)
         resp = self.notify('bootup', boot_checks)
         self.session = resp.read()
         print("Bootup Complete: {0}".format(self.session.decode('utf-8')))
 
     def run(self):
+        if (self.data.checkup_freq < 10):
+            print("Checkup period too low")
+            sys.exit(1)
+        self.checkup_wait = self.data.checkup_freq - 10
+
         self.connect(self.data.mqtt_broker, self.data.mqtt_port, 60)
-        
         self.loop_start()
-        
+        print("Waiting for other bootups...")
+        time.sleep(60)
         self.notify_bootup()
         
         while True:
             self.checkups = {}
+            self.notify_checkup = {}
             self.publish("reporter/checkup_req")
             print("Notify Requested.")
+            self.pings += 1
             time.sleep(10)
-            self.notify_checkup = {}
             for checkup in self.checkups.values():
-                print (checkup)
                 self.notify_checkup.update(checkup)
+            self.notify_checkup["time"] = time.time()
+            print(self.notify_checkup)
+            if (self.pings % self.data.long_checkup_freq == 0):
+                self.pings = 0
+                i = 0
+                for check_name in self.data.boot_check_list:
+                    i += 1
+                    if i > self.data.long_checkup_leng:
+                        break
+                    self.notify_checkup[check_name] = subprocess.check_output(self.data.boot_check_list[check_name], shell=True).decode('utf-8')
             resp = self.notify('checkup', self.notify_checkup)
             print ("Response: " + resp.read().decode('utf-8'))
-            time.sleep(30)
+            time.sleep(self.checkup_wait)
